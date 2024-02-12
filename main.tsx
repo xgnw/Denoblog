05c80788bf9c66e92738ca5ad7a103e8151c0724
xgnw <60925426+xgnw@users.noreply.github.com>
2024-02-12 21:52:40 +0800

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,15 @@
+import blog from "https://deno.land/x/blog/blog.tsx";
+
+blog({
+  author: "小威同学",
+  title: "小威同学的自留地",
+  description: "小威同学的EMO日志。",
+  avatar: "https://deno-avatar.deno.dev/avatar/83a531.svg",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "Email", url: "mailto:1223112302@qq.com" },
+    { title: "GitHub", url: "https://github.com/babiwawa" },
+    { title: "QQ", url: "https://qq.com/1223112302" },
+  ],
+  lang: "zh",
+});
